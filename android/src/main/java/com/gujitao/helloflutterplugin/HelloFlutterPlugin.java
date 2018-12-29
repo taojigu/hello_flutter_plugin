b2c11158bee105739d39e795efb5786e19eaae7b
gujitao001 <gujitao001@ke.com>
2018-12-29 15:51:53 +0800

    add project

---
+++ android/src/main/java/com/gujitao/helloflutterplugin/HelloFlutterPlugin.java
@@ -0,0 +1,25 @@
+package com.gujitao.helloflutterplugin;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** HelloFlutterPlugin */
+public class HelloFlutterPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "hello_flutter_plugin");
+    channel.setMethodCallHandler(new HelloFlutterPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/HelloFlutterPlugin.m
@@ -0,0 +1,20 @@
+#import "HelloFlutterPlugin.h"
+
+@implementation HelloFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"hello_flutter_plugin"
+            binaryMessenger:[registrar messenger]];
+  HelloFlutterPlugin* instance = [[HelloFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
